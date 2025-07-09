5b6876c8fe62806ea4c29abf3ad6d294349ba497
Yared Gezahagn <gabush576@gmail.com>
2025-07-09 15:25:41 +0300

    update
    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ddf2dd0aec5d70553a6277bac58628e2
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "910619b96d28db89c76feebfffeeea74",
+"flutter_bootstrap.js": "bd855d43aea61a98ee3c4d0a4511c7f7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
